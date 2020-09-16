e1def3530e475122e142581b864bd5ad82fabb2f
starkhv70 <starkhv@yandex.ru>
2020-09-16 21:51:03 +1000

    step2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,8 @@ import program from 'commander';
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format');
 
 program.parse(process.argv);
