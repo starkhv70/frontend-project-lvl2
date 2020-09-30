314ab4c9e70409a35da8830a35c59c50d7aeeb6c
BKM <starkhv@yandex.ru>
2020-09-30 16:03:55 +1000

    add test for nested structure

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -8,14 +8,17 @@ import gendiff from '../src/gendiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const files = [['file1.json', 'file2.json'], ['file1.yml', 'file2.yml'], ['file1.ini', 'file2.ini']];
+const files = [['file1.json', 'file2.json', 'result.txt'],
+  ['file1.yml', 'file2.yml', 'result.txt'],
+  ['file1.ini', 'file2.ini', 'result.txt'],
+  ['fileNested1.json', 'fileNested2.json', 'resultNested.txt']];
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test.each(files)('compare %s, %s', (fileName1, fileName2) => {
+test.each(files)('compare: %s, %s', (fileName1, fileName2, fileWithResult) => {
   const filepath1 = getFixturePath(fileName1);
   const filepath2 = getFixturePath(fileName2);
-  const result = readFile('result.txt');
+  const result = readFile(fileWithResult);
   expect(gendiff(filepath1, filepath2)).toEqual(result);
 });
