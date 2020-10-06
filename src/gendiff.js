fc3d6532ad977fd9286792c8ca2b1942c70899d2
starkhv70 <starkhv@yandex.ru>
2020-10-06 22:53:31 +1000

    refactoring function generateOutputStr

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,7 +1,8 @@
 import _ from 'lodash';
 import parse from './parsers.js';
 
-const formatStr = (indent, type, line) => `${' '.repeat(indent)}${type} ${line.key}: ${line.value}`;
+const initialIndent = 2;
+const padding = 4;
 
 const buildDiff = (obj1, obj2) => {
   const keys = _.union(Object.keys(obj1), Object.keys(obj2)).sort();
@@ -13,59 +14,44 @@ const buildDiff = (obj1, obj2) => {
 
     if (keyInObj1 && keyInObj2) {
       if (obj1HasChild && obj2HasChild) {
-        return { type: 'unchanged', key, children: buildDiff(obj1[key], obj2[key]) };
+        return { type: ' ', key, children: buildDiff(obj1[key], obj2[key]) };
       }
       if (obj1[key] === obj2[key]) {
-        return { type: 'unchanged', key, value: obj1[key] };
+        return { type: ' ', key, value: obj1[key] };
       }
       if (obj1HasChild) {
-        return [{ type: 'remove', key, children: buildDiff(obj1[key], obj1[key]) }, { type: 'add', key, value: obj2[key] }];
+        return [{ type: '-', key, children: buildDiff(obj1[key], obj1[key]) }, { type: '+', key, value: obj2[key] }];
       }
       if (obj2HasChild) {
-        return [{ type: 'remove', key, value: obj1[key] }, { type: 'add', key, children: buildDiff(obj2[key], obj2[key]) }];
+        return [{ type: '-', key, value: obj1[key] }, { type: '+', key, children: buildDiff(obj2[key], obj2[key]) }];
       }
-      return [{ type: 'remove', key, value: obj1[key] }, { type: 'add', key, value: obj2[key] }];
+      return [{ type: '-', key, value: obj1[key] }, { type: '+', key, value: obj2[key] }];
     }
     if (keyInObj1) {
       if (obj1HasChild) {
-        return { type: 'remove', key, children: buildDiff(obj1[key], obj1[key]) };
+        return { type: '-', key, children: buildDiff(obj1[key], obj1[key]) };
       }
-      return { type: 'remove', key, value: obj1[key] };
+      return { type: '-', key, value: obj1[key] };
     }
     if (obj2HasChild) {
-      return { type: 'add', key, children: buildDiff(obj2[key], obj2[key]) };
+      return { type: '+', key, children: buildDiff(obj2[key], obj2[key]) };
     }
-    return { type: 'add', key, value: obj2[key] };
+    return { type: '+', key, value: obj2[key] };
   });
   return result;
 };
 
 const generateOutputStr = (diff) => {
-  const numOfSpaces = 2;
-  const parseDiff = (lines, indent) => lines.flatMap((line) => {
-    const hasChildren = _.has(line, 'children');
-    switch (line.type) {
-      case 'add':
-        if (hasChildren) {
-          return [`${' '.repeat(indent)}+ ${line.key}: {`, ...parseDiff(line.children, indent + 4), `${' '.repeat(indent)}  }`];
-        }
-        return formatStr(indent, '+', line);
-      case 'remove':
-        if (hasChildren) {
-          return [`${' '.repeat(indent)}- ${line.key}: {`, ...parseDiff(line.children, indent + 4), `${' '.repeat(indent)}  }`];
-        }
-        return formatStr(indent, '-', line);
-      case 'unchanged':
-        if (hasChildren) {
-          return [`${' '.repeat(indent)}  ${line.key}: {`, ...parseDiff(line.children, indent + 4), `${' '.repeat(indent)}  }`];
-        }
-        return formatStr(indent, ' ', line);
-      default:
-        throw new Error(`Unknown type of line: '${line.type}'!`);
-    }
-  }, []);
+  const formatStr = (indent, line) => `${' '.repeat(indent)}${line.type} ${line.key}: ${line.value}`;
+  const formatStrWithChildren = (indent, line, parseChildren) => {
+    const children = parseChildren(line.children, indent + padding);
+    const header = `${' '.repeat(indent)}${line.type} ${line.key}: {`;
+    const bottom = `${' '.repeat(indent)}  }`;
+    return [header, ...children, bottom];
+  };
+  const parseDiff = (lines, indent) => lines.flatMap((line) => (_.has(line, 'children') ? formatStrWithChildren(indent, line, parseDiff) : formatStr(indent, line)));
 
-  const result = parseDiff(diff, numOfSpaces);
+  const result = parseDiff(diff, initialIndent);
   return `{\n${result.join('\n')}\n}`;
 };
 
@@ -73,7 +59,6 @@ const gendiff = (filePath1, filePath2) => {
   const obj1 = parse(filePath1);
   const obj2 = parse(filePath2);
   const diff = buildDiff(obj1, obj2);
-  // console.log(diff);
   return generateOutputStr(diff);
 };
 
