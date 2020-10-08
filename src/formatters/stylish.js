3d3e377776706c116b4f9d5938396ab8a9759ef7
starkhv70 <starkhv@yandex.ru>
2020-10-08 16:44:07 +1000

    refactoring  function buildDiff

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -3,14 +3,29 @@ import _ from 'lodash';
 const initialIndent = 2;
 const padding = 4;
 
+const getSigh = (type) => {
+  switch (type) {
+    case 'unchanged':
+      return ' ';
+    case 'add':
+      return '+';
+    case 'remove':
+      return '-';
+    default:
+      throw new Error(`Unknown  diff line type: '${type}'!`);
+  }
+};
+
+const formatStr = (indent, line) => `${' '.repeat(indent)}${getSigh(line.type)} ${line.name}: ${line.value}`;
+
+const formatStrWithChildren = (indent, line, parseChildren) => {
+  const children = parseChildren(line.children, indent + padding);
+  const header = `${' '.repeat(indent)}${getSigh(line.type)} ${line.name}: {`;
+  const bottom = `${' '.repeat(indent)}  }`;
+  return [header, ...children, bottom];
+};
+
 export default (diff) => {
-  const formatStr = (indent, line) => `${' '.repeat(indent)}${line.type} ${line.key}: ${line.value}`;
-  const formatStrWithChildren = (indent, line, parseChildren) => {
-    const children = parseChildren(line.children, indent + padding);
-    const header = `${' '.repeat(indent)}${line.type} ${line.key}: {`;
-    const bottom = `${' '.repeat(indent)}  }`;
-    return [header, ...children, bottom];
-  };
   const parseDiff = (lines, indent) => lines.flatMap((line) => (_.has(line, 'children') ? formatStrWithChildren(indent, line, parseDiff) : formatStr(indent, line)));
 
   const result = parseDiff(diff, initialIndent);
--- src/gendiff.js
+++ src/gendiff.js
@@ -2,47 +2,41 @@ import _ from 'lodash';
 import parse from './parsers.js';
 import getFormatter from './formatters/index.js';
 
+const makeNode = (type, name, parseChildren, ...values) => {
+  const [value1] = values;
+  const value2 = (values.length === 2) ? values[1] : value1;
+  if (_.isObject(value1)) {
+    return { type, name, children: parseChildren(value1, value2) };
+  }
+  return { type, name, value: value1 };
+};
+
 const buildDiff = (obj1, obj2) => {
   const keys = _.union(Object.keys(obj1), Object.keys(obj2)).sort();
-  const result = keys.flatMap((key) => {
+  return keys.flatMap((key) => {
     const keyInObj1 = _.has(obj1, key);
     const keyInObj2 = _.has(obj2, key);
-    const obj1HasChild = _.isObject(obj1[key]);
-    const obj2HasChild = _.isObject(obj2[key]);
-
     if (keyInObj1 && keyInObj2) {
-      if (obj1HasChild && obj2HasChild) {
-        return { type: ' ', key, children: buildDiff(obj1[key], obj2[key]) };
-      }
-      if (obj1HasChild) {
-        return [{ type: '-', key, children: buildDiff(obj1[key], obj1[key]) }, { type: '+', key, value: obj2[key] }];
-      }
-      if (obj2HasChild) {
-        return [{ type: '-', key, value: obj1[key] }, { type: '+', key, children: buildDiff(obj2[key], obj2[key]) }];
+      if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
+        return makeNode('unchanged', key, buildDiff, obj1[key], obj2[key]);
       }
       if (obj1[key] === obj2[key]) {
-        return { type: ' ', key, value: obj1[key] };
+        return makeNode('unchanged', key, buildDiff, obj1[key]);
       }
-      return [{ type: '-', key, value: obj1[key] }, { type: '+', key, value: obj2[key] }];
+      return [makeNode('remove', key, buildDiff, obj1[key]), makeNode('add', key, buildDiff, obj2[key])];
     }
     if (keyInObj1) {
-      if (obj1HasChild) {
-        return { type: '-', key, children: buildDiff(obj1[key], obj1[key]) };
-      }
-      return { type: '-', key, value: obj1[key] };
+      return makeNode('remove', key, buildDiff, obj1[key]);
     }
-    if (obj2HasChild) {
-      return { type: '+', key, children: buildDiff(obj2[key], obj2[key]) };
-    }
-    return { type: '+', key, value: obj2[key] };
+    return makeNode('add', key, buildDiff, obj2[key]);
   });
-  return result;
 };
 
 const gendiff = (filePath1, filePath2, formatType) => {
   const obj1 = parse(filePath1);
   const obj2 = parse(filePath2);
   const diff = buildDiff(obj1, obj2);
+  console.log(diff);
   const formatDiff = getFormatter(formatType);
   return formatDiff(diff);
 };
