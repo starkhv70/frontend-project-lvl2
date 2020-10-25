3d588d6402051696eeddab17ac57c7406ddaf926
starkhv70 <starkhv@yandex.ru>
2020-10-25 23:23:29 +1000

    add snapshot testing

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -25,39 +25,11 @@ test.each(files)('compare: %s, %s output format: %s', (fileName1, fileName2, out
   expect(gendiff(filepath1, filepath2, outputFormat)).toEqual(result);
 });
 
-test('buildiff', () => {
-  const obj1 = { obj: { a: { c: 1, d: 2 }, b: 2, f: 'str' }, g: true };
-  const obj2 = { obj: { a: 4, f: 'str', h: 2 }, g: false, j: { k: 'value' } };
-  const result = [{
-    type: 'updated', key: 'g', value: false, oldValue: true,
-  },
-  { type: 'added', key: 'j', value: { k: 'value' } },
-  {
-    type: 'nested',
-    key: 'obj',
-    children: [
-      {
-        key: 'a',
-        type: 'updated',
-        value: 4,
-        oldValue: { c: 1, d: 2 },
-      },
-      {
-        key: 'b',
-        type: 'removed',
-        value: 2,
-      },
-      {
-        key: 'f',
-        type: 'unchanged',
-        value: 'str',
-      },
-      {
-        key: 'h',
-        type: 'added',
-        value: 2,
-      }],
-  }];
-
-  expect(buildDiff(obj1, obj2)).toEqual(result);
+test('Diff is build correctly', () => {
+  const file1 = readFile('fileNested1.json');
+  const file2 = readFile('fileNested2.json');
+  const obj1 = JSON.parse(file1);
+  const obj2 = JSON.parse(file2);
+  const diff = buildDiff(obj1, obj2);
+  expect(JSON.stringify(diff, null, 2)).toMatchSnapshot();
 });
